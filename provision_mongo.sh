347c2b029ca44347864b3bd61015c8a812c21e71
anderson.nishihara <nishihara@gmail.com>
2016-09-13 16:16:12 -0300

    Automatically expose mongodb.

--- provision_mongo.sh
+++ provision_mongo.sh
@@ -4,11 +4,11 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
-sudo service mongod start
+sudo sed -i -e 's#bindIp.*##g' /etc/mongod.conf
+sudo sed -i -e 's#bind_ip.*##g' /etc/mongod.conf
+sudo service mongod restart
 
 echo "=================================================================="
-echo "Inside the box, comment out the bind_ip line from /etc/mongod.conf."
-echo "-------------------------------------------------------------------"
-echo "#Listen to local interface only. Comment out to listen on all interfaces."
-echo "bind_ip = 127.0.0.1"
-echo "-------------------------------------------------------------------"
+echo "MongoDB started."
+echo "Connect on localhost:27017."
+echo "------------------------------------------------------------------"
